db7fd85d99754b166cb23e83c1bc0374f7536624
Augusto Lescano <augustolescano15@gmail.com>
2025-03-24 08:21:47 -0300

    Se creo una función para obtener la respuesta del jugador y otra función la cuál da un valor al azar simulando ser otro jugador

--- piedra_papel_tirejas.js
+++ piedra_papel_tirejas.js
@@ -1,1 +1,19 @@
-console.log('Llego el lechero!')
+function getComputerChoice(){
+  let randomNumber=Math.random()
+  if(randomNumber<=0.6 && randomNumber>=0.4){
+    console.log('Rock')
+  }
+  else if(randomNumber>0.6){
+    console.log('Paper')
+  }
+  else if(randomNumber<0.4){
+    console.log('Scissors')
+  }
+}
+
+function getHumanChoice(){
+  let humanChoice=prompt('Enter one of the options[Rock, Pape, Scissors]: ')
+  console.log(humanChoice);
+}
+
+getHumanChoice();
